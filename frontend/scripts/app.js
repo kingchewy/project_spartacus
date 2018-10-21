589a68e3af6b3e67917ce62167c70a1f99d28202
Unknown <florianschaefer2@gmail.com>
2018-10-21 14:21:00 +0200

    Erster Eindruck Story-Dialog

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,6 +1,6 @@
 App = {
-    go: function () {
-        document.getElementById("logo").addEventListener("animationend", function(){
+    opener: function () {
+        document.getElementsByTagName("use")[0].addEventListener("animationend", function(){
             document.getElementsByClassName("stroke-1")[0].style.strokeWidth = "0px"
             document.getElementsByClassName("stroke-2")[0].style.strokeWidth = "0px"
             document.getElementsByClassName("stroke-3")[0].style.strokeWidth = "0px"
@@ -9,6 +9,7 @@ App = {
             document.getElementsByClassName("stroke-2")[1].style.strokeWidth = "0px"
             document.getElementsByClassName("stroke-3")[1].style.strokeWidth = "0px"
             document.getElementsByClassName("stroke-4")[1].style.strokeWidth = "0px"
+            document.getElementById("opening").remove()
         })
         setTimeout(function(){
                 document.body.style.backgroundPosition = "bottom"
