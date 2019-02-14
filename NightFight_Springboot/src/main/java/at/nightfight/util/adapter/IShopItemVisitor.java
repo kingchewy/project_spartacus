77f6e5f4fdb2fe3c15d73d5468d8dc79cf2dfa5e
Unknown <pa17l013@technikum-wien.at>
2019-02-14 08:04:16 +0100

    Move to "util.adapter"

--- NightFight_Springboot/src/main/java/at/nightfight/util/adapter/IShopItemVisitor.java
+++ NightFight_Springboot/src/main/java/at/nightfight/util/adapter/IShopItemVisitor.java
@@ -1,4 +1,8 @@
-package at.nightfight.model;
+package at.nightfight.util.adapter;
+
+import at.nightfight.model.ShopItemArmor;
+import at.nightfight.model.ShopItemSpecial;
+import at.nightfight.model.ShopItemWeapon;
 
 public interface IShopItemVisitor<T> {
     T visitShopItemWeapon(ShopItemWeapon shopItemWeapon);
